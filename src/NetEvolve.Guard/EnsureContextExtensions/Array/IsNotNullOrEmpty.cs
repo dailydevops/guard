b8c20c97ff3c80e2114a9683d9173f082fbc31a7
Martin Stühmer <me@samtrion.net>
2023-01-06 16:20:05 +0100

    chore: Added Overload for `IsNotNullOrEmpty(Array)`

---
+++ src/NetEvolve.Guard/EnsureContextExtensions/Array/IsNotNullOrEmpty.cs
@@ -0,0 +1,28 @@
+﻿namespace NetEvolve.Guard;
+
+using System;
+using System.Diagnostics;
+
+public partial class EnsureContextExtensions
+{
+    [DebuggerStepThrough]
+    [StackTraceHidden]
+    public static EnsureContext<T[]> IsNotNullOrEmpty<T>(in this EnsureContext<T[]?> value)
+    {
+#if NET6_0_OR_GREATER
+        ArgumentNullException.ThrowIfNull(value.Value, value.ParameterName);
+#else
+        if (value.Value is null)
+        {
+            throw new ArgumentNullException(value.ParameterName);
+        }
+#endif
+
+        if (value.Value.Length == 0)
+        {
+            throw new ArgumentException(null, value.ParameterName);
+        }
+
+        return new EnsureContext<T[]>(value.Value, value.ParameterName);
+    }
+}
