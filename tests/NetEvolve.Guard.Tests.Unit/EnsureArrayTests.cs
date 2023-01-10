ff96347703a7ffdf75964d7432ef0d2cc80901a0
Martin Stühmer <me@samtrion.net>
2023-01-10 23:01:59 +0100

    chore: Added Tests for `IsNotNullOrEmpty{T}(in EnsureContext{T[]?})`

---
+++ tests/NetEvolve.Guard.Tests.Unit/EnsureArrayTests.cs
@@ -0,0 +1,47 @@
+﻿namespace NetEvolve.Guard.Tests.Unit;
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using Xunit;
+
+[ExcludeFromCodeCoverage]
+public class EnsureArrayTests
+{
+    [Fact]
+    public void NotNullOrEmpty_Null_ArgumentNullException()
+    {
+        string?[]? values = null;
+
+        _ = Assert.Throws<ArgumentNullException>(
+            nameof(values),
+            () => _ = Ensure.That(values).IsNotNullOrEmpty()
+        );
+    }
+
+    [Theory]
+    [MemberData(nameof(GetNotNullOrEmptyData))]
+    public void NotNullOrEmpty_Theory_Expected(bool throwException, string?[] values)
+    {
+        if (throwException)
+        {
+            _ = Assert.Throws<ArgumentException>(
+                nameof(values),
+                () => _ = Ensure.That(values).IsNotNullOrEmpty()
+            );
+        }
+        else
+        {
+            _ = Ensure.That(values).IsNotNullOrEmpty();
+        }
+    }
+
+    public static TheoryData GetNotNullOrEmptyData =>
+        new TheoryData<bool, string?[]>
+        {
+            { true, Array.Empty<string?>() },
+            { false, new string?[] { "Hello", null, "World!" } },
+            { false, new string?[] { "Hello", string.Empty, "World!" } },
+            { false, new string?[] { "Hello", " ", "World!" } },
+        };
+}
